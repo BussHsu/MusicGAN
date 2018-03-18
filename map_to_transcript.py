a9aef4f14299d69ed5b979e8cf62a64a9a4d407e
BussHsu <s955207@yahoo.com.tw>
2018-03-17 22:34:42 -0700

    arbitrary length (still working on it)

--- map_to_transcript.py
+++ map_to_transcript.py
@@ -7,13 +7,13 @@ forward_dict = eval(buf)
 backward_dict = {}
 for x,y in forward_dict.items():
     backward_dict[y] = x
-    print str(x)+' : '+str(y)
+    # print str(x)+' : '+str(y)
 
-with open('./Data/generated/1.txt', 'r') as f:
+with open('./Data/a.txt', 'r') as f: # input src
     buf = f.read()
 
 toks = buf.split()
 
-with open('./Data/result/1.txt', 'w') as f:
+with open('./Data/result/a.txt', 'w') as f: # output
     for tok in toks:
         f.write(backward_dict[int(tok)])
